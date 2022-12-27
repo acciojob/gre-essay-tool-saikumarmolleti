76318d0e50fc553613a25ecfa021796b8954f9e0
Saikumar Molleti <80804527+saikumarmolleti@users.noreply.github.com>
2022-12-28 00:12:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+
+function countWord() {
+
+    let sentence = document.getElementById("evaluatedText").value;
+    let counter = 0;
+    let arrayOfWords = sentence.split(' ');
+
+    words = arrayOfWords.filter( word => word!=='');
+    
+    words.forEach( word => counter++);
+
+    console.log(counter);
+    display = document.getElementById("wordCount");
+    display.innerHTML =counter;
+}
